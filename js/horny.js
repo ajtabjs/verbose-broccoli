9b69761c240a4d26bf4a341355a4e6fb1e62a146
aj <134237782+ajtabjs@users.noreply.github.com>
2026-04-19 20:46:48 -0700

    upd

--- js/horny.js
+++ js/horny.js
@@ -1,4 +1,6 @@
 window.nsfwGames = [
   { id: "mfr", label: "my femboy roommate" },
-  { id: "chasingtails", label: "chasing tails a promise in the snow" }
+  { id: "chasingtails", label: "chasing tails a promise in the snow" },
+  { id: "bb", label: "beat banger" },
+  { id: "swh", label: "swh" }
 ];
